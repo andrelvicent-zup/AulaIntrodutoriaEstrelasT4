569a2f8cf296a5f9ad5ec38a1ea369dbe9c6ee99
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-10 14:50:51 -0300

    Criar primeiro componente

---
+++ src/componentes/PrimeiroComponente.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function PrimeiroComponente(){
+    return (
+        <div>
+            <h1> Hello, World!! </h1>
+            <h2> Bem vindo as aulas de react! </h2>
+        </div>
+    )
+}
+
+export default PrimeiroComponente;
