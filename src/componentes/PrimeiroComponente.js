7c07deef42e575fa1e0170ea910aac30576aec57
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-10 15:30:19 -0300

    Criar componente utilizando props

--- src/componentes/PrimeiroComponente.js
+++ src/componentes/PrimeiroComponente.js
@@ -4,7 +4,7 @@ function PrimeiroComponente(){
     return (
         <div>
             <h1> Hello, World!! </h1>
-            <h2> Bem vindo as aulas de react! </h2>
+            <h2> Bem vindo as aulas de xablau! </h2>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ComponenteComProps from './componentes/ComponenteComProps';
 import PrimeiroComponente from './componentes/PrimeiroComponente';
 import './index.css';
 
 ReactDOM.render(
   <div>
-    <PrimeiroComponente/>
+    <ComponenteComProps nome="André" cargo="Instrutor"/>
+    <ComponenteComProps nome="Vini" cargo="Instrutor"/>
+    <ComponenteComProps nome="Maga" cargo="GP"/>
+    <ComponenteComProps nome="Gi" cargo="GP"/>
   </div>
 ,document.getElementById('root')
 );
